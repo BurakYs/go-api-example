d810a7fbd2c19dba4a54661e637abf903a67f6f0
BurakYs <brkyhs0@gmail.com>
2025-05-07 21:05:00 +0300

    Add 404 and 5xx handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/BurakYs/GoAPIExample/config"
 	"github.com/BurakYs/GoAPIExample/db"
+	"github.com/BurakYs/GoAPIExample/models"
 	"github.com/BurakYs/GoAPIExample/routes/userroute"
 
 	"github.com/gin-gonic/gin"
@@ -22,7 +25,28 @@ func main() {
 
 	router := gin.Default()
 
+	router.Use(recovery())
 	userroute.RegisterRoutes(router)
 
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, models.APIError{
+			Message: "Page not found",
+		})
+	})
+
 	router.Run(":" + config.AppConfig.Port)
+}
+
+func recovery() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				ctx.AbortWithStatusJSON(http.StatusInternalServerError, models.APIError{
+					Message: "Internal server error",
+				})
+			}
+		}()
+
+		ctx.Next()
+	}
 }
