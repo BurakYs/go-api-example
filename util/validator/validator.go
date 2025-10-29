8c080334bb3ec1d23d83c5ebf06e1ecb0dc8d750
BurakYs <brkyhs0@gmail.com>
2025-10-30 00:52:16 +0300

    Simplify getErrorMessage() in validator by returning messages directly

--- util/validator/validator.go
+++ util/validator/validator.go
@@ -66,38 +66,34 @@ func ToResponse(ve govalidator.ValidationErrors, location string) ValidationErro
 }
 
 func getErrorMessage(fieldError govalidator.FieldError) string {
-	var msg string
-
 	switch fieldError.Tag() {
 	case "required":
-		msg = "This field is required"
+		return "This field is required"
 	case "email":
-		msg = "This field must be a valid email address"
+		return "This field must be a valid email address"
 	case "uuid":
-		msg = "This field must be a valid UUID"
+		return "This field must be a valid UUID"
 	case "alpha_space":
-		msg = "This field can only contain alphabetic and space characters"
+		return "This field can only contain alphabetic and space characters"
 	case "min":
 		switch fieldError.Kind() {
 		case reflect.String:
-			msg = fmt.Sprintf("This field must be at least %s characters long", fieldError.Param())
+			return fmt.Sprintf("This field must be at least %s characters long", fieldError.Param())
 		case reflect.Slice, reflect.Array:
-			msg = fmt.Sprintf("This field must contain at least %s items", fieldError.Param())
+			return fmt.Sprintf("This field must contain at least %s items", fieldError.Param())
 		default:
-			msg = fmt.Sprintf("The value must be at least %s", fieldError.Param())
+			return fmt.Sprintf("The value must be at least %s", fieldError.Param())
 		}
 	case "max":
 		switch fieldError.Kind() {
 		case reflect.String:
-			msg = fmt.Sprintf("This field must be at most %s characters long", fieldError.Param())
+			return fmt.Sprintf("This field must be at most %s characters long", fieldError.Param())
 		case reflect.Slice, reflect.Array:
-			msg = fmt.Sprintf("This field must contain at most %s items", fieldError.Param())
+			return fmt.Sprintf("This field must contain at most %s items", fieldError.Param())
 		default:
-			msg = fmt.Sprintf("The value must be at most %s", fieldError.Param())
+			return fmt.Sprintf("The value must be at most %s", fieldError.Param())
 		}
 	default:
-		msg = fmt.Sprintf("This field is invalid for: %s", fieldError.Tag())
+		return fmt.Sprintf("This field is invalid for: %s", fieldError.Tag())
 	}
-
-	return msg
 }
